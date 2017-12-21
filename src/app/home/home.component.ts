1e82282359a1e38152c834fa158e7cbf5e471323
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-21 17:54:36 +0000

    mocking articles endpoint

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+ articles = [
+  {
+    "slug": "how-to-train-your-dragon",
+    "title": "How to train your dragon",
+    "description": "Ever wonder how?",
+    "body": "Very carefully.",
+    "createdAt": "2017-12-20T16:09:30.713Z",
+    "updatedAt": "2017-12-20T16:09:30.713Z",
+    "tagList": [
+        "dragons",
+        "training"
+    ],
+    "favoritesCount": 0,
+    "author": {
+        "username": "John Doe",
+        "image": "https://static.productionready.io/images/smiley-cyrus.jpg",
+        "following": false
+    }
+},
+{
+  "slug": "increase-your-productivity",
+  "title": "Increase your productivity",
+  "description": "Ever wonder how?",
+  "body": "Very carefully.",
+  "createdAt": "2016-01-20T16:09:30.713Z",
+  "updatedAt": "2017-12-20T16:09:30.713Z",
+  "tagList": [
+      "productivity",
+      "training"
+  ],
+  "favoritesCount": 0,
+  "author": {
+      "username": "Robert Dinero",
+      "image": "https://static.productionready.io/images/smiley-cyrus.jpg",
+      "following": false
+  }
+}
+,
+{
+  "slug": "",
+  "title": "How to train your dragon",
+  "description": "Ever wonder how?",
+  "body": "Very carefully.",
+  "createdAt": "2017-12-20T16:09:30.713Z",
+  "updatedAt": "2017-12-20T16:09:30.713Z",
+  "tagList": [
+      "dragons",
+      "training"
+  ],
+  "favoritesCount": 0,
+  "author": {
+      "username": "Antoine grizly ",
+      "image": "https://static.productionready.io/images/smiley-cyrus.jpg",
+      "following": false
+  }
+}
+ ]
   constructor() { }
 
   ngOnInit() {
