96d4ad6929dc84741ea247c2d1fcdbca8ad5d34e
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-21 18:30:11 +0000

    adding articles retreival

--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import { Headers, Http } from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+// Import RxJs required methods
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
 const ENDPOINT = environment.ApiUrl+"articles"
 @Injectable()
 export class ArticlesService {
   
-  constructor() { }
+  constructor(private http : Http) { }
+  articles(){
+    return this.http.get(ENDPOINT).map(res =>res.json()).catch(null);
+  }
 
+  handleError(error){
+    // log Error in a log file or send the error to a third party
+    Observable.throw(error);
+    
+  }
 }
