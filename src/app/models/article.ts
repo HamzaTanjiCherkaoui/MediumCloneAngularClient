911605c4d4ad8b02dab341fb1014a44e0233dc63
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-21 18:11:23 +0000

    WIP : models

--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -1,2 +1,12 @@
+import {Author} from './author'
 export class Article {
+    slug:String
+    description : String
+    body : String 
+    favoritesCount : Number
+    comments : [{String}]
+    taglist : [{String}]
+    author : Author
+    
+
 }
