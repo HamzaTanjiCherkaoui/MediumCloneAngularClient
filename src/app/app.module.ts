d8ae34a658a0c9d06de3d5ae4f8889fcd2311c0d
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-21 16:11:10 +0000

    adding single article component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import {RoutesModule} from './app.routes'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { SingleArticleComponent } from './single-article/single-article.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    SingleArticleComponent
   ],
   imports: [
     BrowserModule,
