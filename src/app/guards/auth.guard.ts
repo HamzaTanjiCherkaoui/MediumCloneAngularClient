b4e2f54580afe33ab860635bf134cbb24a43438e
HamzaTanjiCherkaoui <hoxa1235@gmail.com>
2017-12-23 22:25:20 +0000

    moving header and footer from the home component to the app component , and adding auth guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot , Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { CookieService } from 'ngx-cookie';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  constructor(private router: Router, private cookies: CookieService) { }
+  
+  canActivate() {
+    if (this._isLogin())
     return true;
+    else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+
+  _isLogin(): Boolean {
+    if (this.cookies.get('auth_token'))
+    return true;
+    else
+    return false;
   }
 }
